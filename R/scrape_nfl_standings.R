24a32687d0135d5f05c6c58401c35a545f3dbee7
Thomas Mock <j.thomasmock@gmail.com>
2020-05-07 13:29:28 -0500

    add html_nodes("img") to get all team logos

--- R/scrape_nfl_standings.R
+++ R/scrape_nfl_standings.R
@@ -40,7 +40,7 @@ scrape_nfl_standings <- function(season, add_superbowls = FALSE){
   team_names <- raw_season %>%
     html_node("div.tabs__content") %>%
     html_nodes("span.pr4.TeamLink__Logo") %>%
-    html_nodes("a.AnchorLink > img") %>%
+    html_nodes("img") %>%
     html_attr("alt")
 
   team_abb <- raw_season %>%
