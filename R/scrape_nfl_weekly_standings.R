3d99f883f4d4a6981e65b177783a12adf2bee101
Thomas Mock <j.thomasmock@gmail.com>
2020-09-16 09:10:29 -0500

    convert back to NA for ties

--- R/scrape_nfl_weekly_standings.R
+++ R/scrape_nfl_weekly_standings.R
@@ -65,7 +65,7 @@ scrape_nfl_weekly_standings <- function(season = 2020, tidy = FALSE) {
       winner = case_when(
         pts_winner > pts_loser ~ 1,
         pts_winner < pts_loser ~ 0,
-        pts_winner == pts_loser ~ "TIE",
+        pts_winner == pts_loser ~ NA_real_,
         TRUE ~ NA_real_
         )
       ) %>%
